4fe8e84225110333e8509afc0d91c087812b2f92
conradlauren-creator <conradlauren924@gmail.com>
2026-03-24 21:32:03 -0400

    last modified header, check if files inside root directory

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ class HTTPServer:
     s = None
     server_root = "server_files"
 
+    @staticmethod
     def main():
         for res in socket.getaddrinfo(HTTPServer.HOST, HTTPServer.PORT, socket.AF_UNSPEC,
                                     socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
@@ -71,51 +72,63 @@ class HTTPServer:
                     stripped_path = path.lstrip("/")
                     full_path = os.path.join(HTTPServer.server_root, stripped_path)
 
-        #FEATURE: Secure directory so that users cannot access elements outside the servers files            
                 
                 # FORBIDDEN: 403 ERROR
-                    if "..\\" in path or "../" in path:
+
+                    abs_root = os.path.abspath(HTTPServer.server_root)
+                    abs_full = os.path.abspath(full_path)
+
+                    if "..\\" in path or "../" in path or not abs_full.startswith(abs_root):
                         print("403 Forbidden")
                         firstLine = "403 Forbidden"
-                        response = HTTPServer.responses(firstLine, "")
+                        response = HTTPServer.responses(firstLine, "", "")
+
                         
                 #˙⊹° GET.°⊹˙⋆🖳₊˚⊹.
                     elif method == 'GET':
                         if os.path.exists(full_path):
                             body = Path(full_path).read_text()
-
+                            timestamp = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+                            
                             firstLine = "200 OK"
-                            response = HTTPServer.responses(firstLine, body)
+                            response = HTTPServer.responses(firstLine, body, timestamp)
+
+
 
                         else:
+                            # NOT FOUND : 404 ERROR
                             #note: it's prob always gonna produce a 404 error for favicon since we don't have one
                             print(full_path, " was expected but couldn't be found.\n")
                             firstLine = "404 Not Found"
-                            response = HTTPServer.responses(firstLine, "")
+                            response = HTTPServer.responses(firstLine, "", "")
                             
 
                 #˙⊹° HEAD.°⊹˙⋆🖳₊˚⊹.
                     elif method == 'HEAD':
                         if os.path.exists(full_path):
                             body = Path(full_path).read_text()
-
+                            timestamp = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+                            
                             firstLine = "200 OK"
-                            response = HTTPServer.responses(firstLine, "")
+                            response = HTTPServer.responses(firstLine, "", timestamp)
+
 
                         else:
                             print(full_path, " was expected but couldn't be found.")
                             firstLine = "404 Not Found"
-                            response = HTTPServer.responses(firstLine, "")
+                            response = HTTPServer.responses(firstLine, "", "")
 
                 # NEITHER: 501 ERROR
                     else:
                         print("501 Not Implemented")
                         firstLine = "501 Not Implemented"
-                        response = HTTPServer.responses(firstLine, "")
+                        response = HTTPServer.responses(firstLine, "", "")
 
                     #responses shared by all 
                     print(raw_http_request)
                     conn.send(response.encode('utf-8'))
+                    if headers.get("Connection", "").lower() == "close":
+                        break
 
         except Exception as e:
             print("Error:", e)
@@ -124,19 +137,34 @@ class HTTPServer:
 
 
     #new method starts here and includes this
-    def responses(pFirstLine, pBody):
+    @staticmethod
+    def responses(pFirstLine, pBody, pLastModified):
 
         date = datetime.now(timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')
 
-        return (
-            f"HTTP/1.1 {pFirstLine}\r\n"
-            f"Content-Type: text/plain\r\n"
-            f"Content-Length: {len(pBody)}\r\n"
-            f"Server: HudsonAndLaurensServer\r\n"
-            f"Date: {date}\r\n"
-            f"\r\n"
-            f"{pBody}"
-            )
-    
+        if pLastModified == "":
+            return (
+                f"HTTP/1.1 {pFirstLine}\r\n"
+                f"Content-Type: text/plain\r\n"
+                f"Content-Length: {len(pBody)}\r\n"
+                f"Server: HudsonAndLaurensServer\r\n"
+                f"Connection: keep-alive\r\n"
+                f"Date: {date}\r\n"
+                f"\r\n"
+                f"{pBody}"
+                )
+        else: 
+             return (
+                f"HTTP/1.1 {pFirstLine}\r\n"
+                f"Content-Type: text/plain\r\n"
+                f"Content-Length: {len(pBody)}\r\n"
+                f"Last-Modified: {pLastModified}\r\n"
+                f"Server: HudsonAndLaurensServer\r\n"
+                f"Connection: keep-alive\r\n"
+                f"Date: {date}\r\n"
+                f"\r\n"
+                f"{pBody}"
+                )
+        
 
 HTTPServer.main()
